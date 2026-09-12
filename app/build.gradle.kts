d46c0a15421a1a9d8d989ce39526adcbc2ae11a6
s2946528 <s2946528@gmail.com>
2026-09-12 17:58:15 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
   compileSdk { version = release(36) { minorApiLevel = 1 } }
 
   defaultConfig {
-    applicationId = "com.aistudio.dokkanipos.dkay"
+    applicationId = "com.aistudio.dokkanipos.dkay.v2"
     minSdk = 24
     targetSdk = 36
     versionCode = 1
